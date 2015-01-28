73ccaaf45d5a737187e55afb6822fcfea28be634
Gjum <gjum42@gmail.com>
2015-01-28 22:56:51 +0100

    Print human-readable status on received_entity_status()

--- bot.py
+++ bot.py
@@ -536,6 +536,33 @@ class BotProtocol(ClientProtocol):
         status = buff.unpack('b')
         if status == 2: # Living Entity hurt, sent on health change
             self.tasks.add_delay(0.1, self.send_player_position) # send position update to receive health update
+        entity_status_dict = {
+                 0: "unknown: Something related to living entities?",
+                 1: "unknown: Something related to the player entity?",
+                 2: "Living Entity hurt",
+                 3: "Living Entity dead",
+                 4: "Iron Golem throwing up arms",
+                 6: "Wolf/Ocelot/Horse taming - Spawn heart particles",
+                 7: "Wolf/Ocelot/Horse tamed - Spawn smoke particles",
+                 8: "Wolf shaking water - Trigger the shaking animation",
+                 9: "(of self) Eating accepted by server",
+                10: "Sheep eating grass",
+                10: "Play TNT ignite sound",
+                11: "Iron Golem handing over a rose",
+                12: "Villager mating - Spawn heart particles",
+                13: "Spawn particles indicating that a villager is angry and seeking revenge",
+                14: "Spawn happy particles near a villager",
+                15: "Witch animation - Spawn magic particles",
+                16: "Play zombie converting into a villager sound",
+                17: "Firework exploding",
+                18: "Animal in love (ready to mate) - Spawn heart particles",
+                19: "Reset squid rotation",
+                20: "Spawn explosion particle - works for some living entities",
+                21: "Play guardian sound - works for every entity",
+                22: "Enables reduced debug for players",
+                23: "Disables reduced debug for players",
+        }
+        print 'entity_status: eid', eid, entity_status_dict[status]
 
     ### blocks ###
 
