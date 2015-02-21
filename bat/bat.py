a14e47e87ce55513d606446d61bd5afbe89d20ec
Gjum <gjum42@gmail.com>
2015-02-21 03:13:52 +0100

    Implement `tp, dig, place, close, show hotbar, drop` commands

--- bat/bat.py
+++ bat/bat.py
@@ -25,6 +25,8 @@ class BatPlugin:
 
 		self.clinfo = ploader.requires('ClientInfo')
 		self.inventory = ploader.requires('Inventory')
+		self.net = ploader.requires('Net')
+		self.timer = ploader.requires('Timers')
 		self.world = ploader.requires('World')
 		self.command_handler = CommandRegistry(self)
 
@@ -58,7 +60,10 @@ class BatPlugin:
 				'dig coords',
 				'place coords',
 				'int interact_coords',
+				'close',
 				'select slot_index',
+				'showhot',
+				'drop slot=None, drop_stack=""',
 				'plan',
 				'hold item_id meta=-1',
 				'hotbar *prepare_args',
@@ -69,31 +74,65 @@ class BatPlugin:
 
 	@register_command('tp', '3')
 	def tp(self, coords):
-		self.clinfo.set_position(coords)
+		pos = self.clinfo.position
+		pos.x, pos.y, pos.z = map(float, coords)
 
 	@register_command('tpb', '3')
 	def tp_block(self, coords):
-		self.clinfo.set_position(block_coords(coords))
+		pos = self.clinfo.position
+		pos.x, pos.y, pos.z = map(lambda c: c + 0.5, block_coords(coords))
+		pos.y -= 0.5
 
 	@register_command('gravity')
 	def gravity(self):
 		logger.warn('TODO')
 
 	@register_command('dig', '3')
-	def dig(self, coords):
-		logger.warn('TODO')
+	def dig_block(self, coords):
+		packet = {
+			'status': 0,  # start
+			'location': Vec3(*coords).get_dict(),
+			'face': 1,
+		}
+		self.net.push_packet('PLAY>Player Digging', packet)
+		packet['status'] = 2  # finish
+		self.net.push_packet('PLAY>Player Digging', packet)
 
 	@register_command('place', '3')
-	def place(self, coords):
-		logger.warn('TODO')
+	def place_block(self, coords):
+		packet = {
+			'location': Vec3(*coords).get_dict(),
+			'direction': 1,
+			'held_item': self.inventory.get_held_item().get_packet_data(),
+			'cur_pos_x': 8,
+			'cur_pos_y': 8,
+			'cur_pos_z': 8,
+		}
+		self.net.push_packet('PLAY>Player Block Placement', packet)
 
 	@register_command('int', '3')
 	def interact(self, coords):
 		self.inventory.interact_with_block(Vec3(*coords))
 
+	@register_command('close')
+	def close_window(self):
+		self.inventory.close_window()
+
 	@register_command('select', '1')
 	def select_slot(self, slot_index):
 		self.inventory.select_slot(slot_index)
+
+	@register_command('showhot')
+	def show_hotbar(self):
+		self.show_hotbar_slotcounter = 0  # FIXME XXX TODO EVIL HACK
+		def cb():
+			self.inventory.select_slot(self.show_hotbar_slotcounter)
+			self.show_hotbar_slotcounter += 1
+		self.timer.reg_event_timer(0.5, cb, runs=8)
+
+	@register_command('drop', '?1?s')
+	def drop_item(self, slot=None, drop_stack=''):
+		self.inventory.drop_item(slot, len(drop_stack) > 0)
 
 	@register_command('plan')
 	def print_plan(self):
