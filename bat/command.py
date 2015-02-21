7f2bb049fda7e87737ab4d688d3e3b672b3aac90
Gjum <gjum42@gmail.com>
2015-02-22 00:24:48 +0100

    Fix command parser

--- bat/command.py
+++ bat/command.py
@@ -9,6 +9,8 @@ def register_command(cmd, arg_fmt=''):
 	return inner
 
 def _format_args(args, arg_fmt):
+	if arg_fmt == '*':  # raw args, parsed by handler
+		return args
 	def try_cast_num(str_arg):
 		try:
 			return int(str_arg)
@@ -17,32 +19,31 @@ def _format_args(args, arg_fmt):
 	optional = 0
 	pos = 0
 	out = []
-	try:
-		for c in arg_fmt:
-			if c == 's':  # append single str
-				out.append(args[pos])
-				pos += 1
-			elif '0' <= c <= '9':  # append numbers
-				tuple_size = int(c)
-				try:
-					tuple_args = list(map(try_cast_num, args[pos:pos+tuple_size]))
-				except ValueError:  # could not cast int or float
-					if optional > 0:
-						optional -= 1
-					else:  # not optional, but wrong type
-						return None
-				else:  # args successfully casted
-					out.append(tuple_args if len(tuple_args) > 1 else tuple_args[0])
-					pos += tuple_size
-			elif c == '?':  # append next if present
-				optional += 1
-			elif c == '*':  # append any args of same type left
-				optional = len(args)
+	for c in arg_fmt:
+		if c == 's':  # append single str
+			if pos >= len(args):
+				if optional <= 0:
+					return None
 			else:
-				logger.error('[Command] Unknown format: %s in %s', c, arg_fmt)
-		return out
-	except IndexError:  # arguments do not match format
-		return None
+				out.append(args[pos])
+			pos += 1
+		elif '0' <= c <= '9':  # append numbers
+			tuple_size = int(c)
+			try:
+				tuple_args = list(map(try_cast_num, args[pos:pos+tuple_size]))
+				out.append(tuple_args if len(tuple_args) > 1 else tuple_args[0])
+				pos += tuple_size
+			except (ValueError, IndexError):  # could not cast int or float
+				if optional > 0:
+					optional -= 1
+					pos += 1
+				else:  # not optional, but wrong type
+					return None
+		elif c == '?':  # append next if present
+			optional += 1
+		else:
+			logger.error('[Command] Unknown format: %s in %s at %i', c, arg_fmt, pos)
+	return out
 
 class CommandRegistry:
 	""" Class to register commands.
