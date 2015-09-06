cd7050d5b9bba2cfc82eac696742bf2da14318a4
Gjum <code.gjum@gmail.com>
2015-09-06 17:58:18 +0200

    Command plugin handles 'cmd' event from curses

--- bat/command.py
+++ bat/command.py
@@ -1,13 +1,16 @@
 # TODO this module is a deprecated mess
-
 import logging
+
+
 logger = logging.getLogger('spock')
+
 
 def register_command(cmd, arg_fmt=''):
     def inner(fnc):
         fnc._cmd_handler = (cmd, arg_fmt)
         return fnc
     return inner
+
 
 class CommandRegistry:
 
@@ -23,6 +26,7 @@ class CommandRegistry:
                 handler = getattr(cls, field_name)
                 self.cmd_handlers[cmd_with_prefix] = (handler, arg_fmt)
 
+
 class CommandPlugin:
 
     def __init__(self, ploader, settings):
@@ -31,18 +35,40 @@ class CommandPlugin:
         self.event = ploader.requires('Event')
         self.registry = CommandRegistry()
         ploader.provides('Commands', self.registry)
+        ploader.reg_event_handler('cmd', self.handle_cmd)
         ploader.reg_event_handler('chat_any', self.handle_chat)
 
+    def handle_cmd(self, evt, data):
+        cmd, args = data['cmd'], data['args']
+        if cmd not in self.registry.cmd_handlers:
+            return
+        handler, arg_fmt = self.registry.cmd_handlers[cmd]
+        data['name'] = '(Console)'
+        data['sort'] = '(Curses)'
+        formatted_args = self.format_args(args, arg_fmt, data)
+        if formatted_args is None:
+            logger.warn('[Command] <%s via %s> Illegal arguments for %s:'
+                        ' expected %s, got %s',
+                        data['name'], data['sort'], cmd, arg_fmt, args)
+        else:
+            logger.info('[Command] <%s via %s> %s %s',
+                        data['name'], data['sort'], cmd, formatted_args)
+            handler(*formatted_args)
+
     def handle_chat(self, evt, data):
-        cmd, *args = data['text'].split(' ')  # TODO Can /tellraw send empty chat messages? Catch the exception.
+        # TODO Can /tellraw send empty chat messages? Catch the exception.
+        cmd, *args = data['text'].split(' ')
         if cmd not in self.registry.cmd_handlers:
             return
         handler, arg_fmt = self.registry.cmd_handlers[cmd]
         formatted_args = self.format_args(args, arg_fmt, data)
         if formatted_args is None:
-            logger.warn('[Command] <%s via %s> Illegal arguments for %s: expected %s, got %s', data['name'], data['sort'], cmd, arg_fmt, args)
+            logger.warn('[Command] <%s via %s> Illegal arguments for %s: '
+                        'expected %s, got %s',
+                        data['name'], data['sort'], cmd, arg_fmt, args)
         else:
-            logger.info('[Command] <%s via %s> %s %s', data['name'], data['sort'], cmd, formatted_args)
+            logger.info('[Command] <%s via %s> %s %s',
+                        data['name'], data['sort'], cmd, formatted_args)
             handler(*formatted_args)
 
     def format_args(self, args, arg_fmt, data):
@@ -67,10 +93,13 @@ class CommandPlugin:
             elif '0' <= c <= '9':  # append numbers
                 tuple_size = int(c)
                 try:
-                    tuple_args = list(map(try_cast_num, args[pos:pos+tuple_size]))
-                    out.append(tuple_args if len(tuple_args) > 1 else tuple_args[0])
+                    tuple_args = list(map(try_cast_num,
+                                          args[pos:pos+tuple_size]))
+                    out.append(tuple_args if len(tuple_args) > 1
+                               else tuple_args[0])
                     pos += tuple_size
-                except (ValueError, IndexError):  # could not cast int or float
+                except (ValueError, IndexError):
+                    # could not cast int or float
                     if optional > 0:
                         optional -= 1
                         pos += 1
@@ -79,7 +108,8 @@ class CommandPlugin:
             elif c == '?':  # append next if present
                 optional += 1
             elif c == 'e':  # player entity that executed the command
-                if 'uuid' not in data or data['uuid'] is None:  # not executed by a player
+                if 'uuid' not in data or data['uuid'] is None:
+                    # not executed by a player
                     if optional <= 0:
                         return None  # TODO return error message
                 else:
@@ -92,5 +122,6 @@ class CommandPlugin:
                         if optional <= 0:
                             return None
             else:
-                logger.error('[Command] Unknown format: %s in %s at %i', c, arg_fmt, pos)
+                logger.error('[Command] Unknown format: %s in %s at %i',
+                             c, arg_fmt, pos)
         return out
