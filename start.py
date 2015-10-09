e745e9daedfa0d1f6090faa192f701f8dd2f7919
Gjum <code.gjum@gmail.com>
2015-10-10 01:19:49 +0200

    Update start.py for new plugins

--- start.py
+++ start.py
@@ -1,35 +1,12 @@
 from spockbot import Client
-from spockbot.plugins.core import auth, event, net, taskmanager, timer, ticker
-from spockbot.plugins.helpers import (
-    chat, clientinfo, craft, entities, interact, inventory,
-    keepalive, movement, physics, respawn, start, world)
+from spockbot.plugins import default_plugins as plugins
 from bat import bat, command, curses
 
-plugins = [
-    ('auth', auth.AuthPlugin),
-    ('event', event.EventPlugin),
-    ('net', net.NetPlugin),
-    ('taskmanager', taskmanager.TaskManager),
-    ('ticker', ticker.TickerPlugin),
-    ('timer', timer.TimerPlugin),
-
-    ('chat', chat.ChatPlugin),
-    ('clientinfo', clientinfo.ClientInfoPlugin),
-    ('craft', craft.CraftPlugin),
-    ('entities', entities.EntitiesPlugin),
-    ('interact', interact.InteractPlugin),
-    ('inventory', inventory.InventoryPlugin),
-    ('keepalive', keepalive.KeepalivePlugin),
-    ('movement', movement.MovementPlugin),
-    ('physics', physics.PhysicsPlugin),
-    ('respawn', respawn.RespawnPlugin),
-    ('start', start.StartPlugin),
-    ('world', world.WorldPlugin),
-
+plugins.extend([
     ('bat', bat.BatPlugin),
     ('commands', command.CommandPlugin),
     ('curses', curses.CursesPlugin),
-]
+])
 
 # login_credentials should contain a dict with 'username' and 'password'
 #from login_credentials import settings
