f2cbb0ce2be26365c4b2d8b4e5bd937707b295af
Gjum <code.gjum@gmail.com>
2016-01-03 18:41:09 +0100

    Curse plugin moved to CursingSpock repo
    https://github.com/Gjum/CursingSpock

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
+import cursingspock
 from spockbot import Client
 from spockbot.plugins import default_plugins as plugins
-from bat import bat, command, curses
+from bat import bat, command
 
 plugins.extend([
     ('bat', bat.BatPlugin),
     ('commands', command.CommandPlugin),
-    ('curses', curses.CursesPlugin),
+    ('curses', cursingspock.CursesPlugin),
 ])
 
 # login_credentials should contain a dict with 'username' and 'password'
