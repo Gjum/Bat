3853eabb1f9ddcd580134e42aa46933e8b0a53d9
Gjum <code.gjum@gmail.com>
2016-02-22 23:09:38 +0100

    Cleanup

--- bat/bat.py
+++ bat/bat.py
@@ -1,11 +1,12 @@
 import logging
 import traceback
 
+from bat.command import register_command
 from spockbot.mcdata.windows import Slot
 from spockbot.plugins.base import PluginBase
 from spockbot.plugins.tools.task import TaskFailed
 from spockbot.vector import Vector3 as Vec
-from bat.command import register_command
+
 
 logger = logging.getLogger('spockbot')
 
@@ -52,6 +53,7 @@ class TaskChatter(object):
         logger.warn('Task "%s" failed: %s', self.name, trace)
 
 
+# TODO move
 def slot_from_item(item):
     if 2 != getattr(item, 'obj_type', None):
         return None
@@ -63,35 +65,21 @@ def slot_from_item(item):
     return None
 
 
-# noinspection PyUnresolvedReferences
 class BatPlugin(PluginBase):
-    requires = ('Event', 'Net', 'TaskManager', 'Timers',
-                'Chat', 'ClientInfo', 'Entities', 'Interact', 'World',
-                'Craft', 'Commands', 'Inventory')
+    requires = ('Chat', 'Entities', 'Commands', 'InventoryCmd')
     events = {
         'chat': 'log_chat',
         'inventory_open_window': 'log_inventory',
         'inventory_click_response': 'log_inventory',  # xxx log clicked slot
     }
-    # logged events TODO remove
-    events.update({e: 'debug_event' for e in (
-        'LOGIN<Login Success',
-        'PLAY<Sign Editor Open',  # xxx sign placement and writing
-        # TODO find out what /clear does, also when a win is open
-        'PLAY<Window Property', 'inventory_win_prop', 'inventory_close_window',
-    )})
 
     def __init__(self, ploader, settings):
-        # self.capture_args(locals())
         super(BatPlugin, self).__init__(ploader, settings)
-
-        self.clinfo = self.clientinfo
         self.commands.register_handlers(self)
 
-
-    def debug_event(self, evt, data):
-        data = getattr(data, 'data', data)
-        logger.debug('%s: %s', evt, data)
+    @register_command('say', '*')
+    def chat_say(self, *msgs):
+        self.chat.chat(' '.join(msgs))
 
     def log_chat(self, evt, data):
         text = data.get('text', '[Chat] <%s via %s> %s' %
@@ -101,13 +89,14 @@ class BatPlugin(PluginBase):
     def log_inventory(self, *_):
         self.inventorycmd.log_inventory()
 
+    # WIP
     def find_dropped_items(self, wanted=None, near=None):
         """
         If ``near`` is a Vector3, the items are sorted by distance to ``near``.
         """
         items = []
         for item in self.entities.objects:
-            slot = self.slot_from_item(item)
+            slot = slot_from_item(item)
             if not slot:
                 continue
             if near:
@@ -118,11 +107,3 @@ class BatPlugin(PluginBase):
             dist = near.dist_sq
             for d, item, slot in sorted((dist(Vec(i)), i, s) for i, s in items):
                 yield (item, slot)
-
-    @register_command('say', '*')
-    def chat_say(self, *msgs):
-        self.chat.chat(' '.join(msgs))
-
-    @register_command('clone', '333')
-    def clone_area(self, c_from, c_to, c_target):
-        logger.warn('TODO')
