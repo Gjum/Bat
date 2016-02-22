06386823ef0005707d2c0de98ed64584c350af98
Gjum <code.gjum@gmail.com>
2016-02-22 22:28:54 +0100

    ReloadTestPlugin (broken and unused for now)

--- bat/bat.py
+++ bat/bat.py
@@ -1,8 +1,5 @@
-import importlib
 import logging
-import sys
 import traceback
-import types
 
 from spockbot.mcdata import get_item_or_block
 from spockbot.mcdata.windows import Slot
@@ -14,72 +11,6 @@ from bat.command import register_command
 logger = logging.getLogger('spockbot')
 
 reach_dist_sq = 4 * 4
-
-
-class Reloadable(object):
-    """Makes inheriting class instances reloadable."""
-
-    _persistent_attributes = []  # will be kept when reloading
-
-    def capture_args(self, init_args):
-        """
-        Capture original args for calling init on reload.
-        An ancestor should call this in their init with locals() as argument
-        before creating any more local variables:
-
-        >>> class Foo(Reloadable):
-        >>>     def __init__(self, foo, bar=123):
-        >>>         super(self.__class__, self).__init__()
-        >>>         self.foo = foo * bar  # just uses `self`, thus not creating a local var
-        >>>         self.capture_args(locals())  # captures self, foo, bar
-        >>>         tmp = bar ** 2  # creates local var `tmp`
-        >>>         # ...
-        """
-        self._init_args = dict(init_args)
-        for k in list(self._init_args.keys()):
-            if k == 'self' or k[:2] == '__':
-                del self._init_args[k]
-
-    def reload(self, new_module=None):
-        """
-        Reloads the containing module and replaces all instance attributes
-        while keeping the attributes in _persistent_attributes.
-        This is called monkey-patching, see
-        https://filippo.io/instance-monkey-patching-in-python/
-        """
-        if not new_module:
-            new_module = importlib.reload(sys.modules[self.__module__])
-        new_class = getattr(new_module, self.__class__.__name__)
-        persistent = {attr_name: getattr(self, attr_name, None)
-                      for attr_name in self._persistent_attributes}
-        for new_attr_name in dir(new_class):
-            if new_attr_name in ('__class__', '__dict__', '__weakref__'):
-                # do not copy '__dict__', '__weakref__'
-                # but copy '__class__' below
-                continue
-            new_attr = getattr(new_class, new_attr_name)
-            try:  # some attributes are instance methods, bind them to `self`
-                new_attr = types.MethodType(new_attr, self)
-            except TypeError:
-                pass
-            setattr(self, new_attr_name, new_attr)
-        setattr(self, '__class__', new_class)
-        new_class.__init__(self, **getattr(self, '_init_args', {}))
-        for k, v in persistent.items():
-            setattr(self, k, v)
-
-    def try_reload(self):
-        """
-        Try to reload the containing module.
-        If an exception is thrown in the process, catch and return it.
-        Useful to catch syntax errors.
-
-        :return the thrown exception if not successfully reloaded, None otherwise
-        """
-        try:
-            self.reload()
-        except Exception as e:
-            return e
 
 
 class TaskEnder(object):
@@ -136,9 +67,7 @@ def slot_from_item(item):
 
 
 # noinspection PyUnresolvedReferences
-class BatPlugin(PluginBase):#, Reloadable):
-    _persistent_attributes = ('path_queue', 'event')
-
+class BatPlugin(PluginBase):
     requires = ('Event', 'Net', 'TaskManager', 'Timers',
                 'Chat', 'ClientInfo', 'Entities', 'Interact', 'World',
                 'Craft', 'Commands', 'Inventory')
@@ -190,41 +119,6 @@ class BatPlugin(PluginBase):#, Reloadable):
             dist = near.dist_sq
             for d, item, slot in sorted((dist(Vec(i)), i, s) for i, s in items):
                 yield (item, slot)
-
-    @register_command('reload')
-    def reload_now(self):  # xxx broken do not use
-
-        def unregister_handlers():
-            try:
-                for event, handler_name in self.events.items():
-                    handler = getattr(self, handler_name)
-                    if handler in self.event.event_handlers[event]:
-                        self.event.event_handlers[event].remove(handler)
-            except AttributeError:
-                pass
-
-        def do_the_reload():
-            unregister_handlers()
-            # setattr(self, 'reloaded', True)
-            e = self.try_reload()
-            if e is None:
-                unregister_handlers()
-                logger.debug('[Bat] reloaded')
-            else:
-                logger.debug('[Bat] not reloaded: %s', e)
-
-        def wait(time):
-            cb = lambda *_: self.event.emit('sleep_over', {})
-            self.timers.reg_event_timer(time, cb, runs=1)
-            return 'sleep_over'
-
-        def task():
-            logger.debug('before reload 123')
-            do_the_reload()
-            yield wait(1)
-            logger.debug('after reload 234')
-
-        self.taskmanager.run_task(task(), TaskChatter('Reloader', self.chat))
 
     @register_command('say', '*')
     def chat_say(self, *msgs):
---
+++ bat/reloadtest.py
@@ -0,0 +1,124 @@
+import importlib
+import logging
+
+import sys
+import types
+
+from bat.bat import TaskChatter
+from spockbot.plugins.base import pl_announce, PluginBase
+
+
+logger = logging.getLogger('spockbot')
+
+
+class Reloadable(object):
+    """Makes inheriting class instances reloadable."""
+
+    _persistent_attributes = []  # will be kept when reloading
+
+    def capture_args(self, init_args):
+        """
+        Capture original args for calling init on reload.
+        An ancestor should call this in their init with locals() as argument
+        before creating any more local variables:
+
+        >>> class Foo(Reloadable):
+        >>>     def __init__(self, foo, bar=123):
+        >>>         super(self.__class__, self).__init__()
+        >>>         self.foo = foo * bar  # just uses `self`, thus not creating a local var
+        >>>         self.capture_args(locals())  # captures self, foo, bar
+        >>>         tmp = bar ** 2  # creates local var `tmp`
+        >>>         # ...
+        """
+        self._init_args = dict(init_args)
+        for k in list(self._init_args.keys()):
+            if k == 'self' or k[:2] == '__':
+                del self._init_args[k]
+
+    def reload(self, new_module=None):
+        """
+        Reloads the containing module and replaces all instance attributes
+        while keeping the attributes in _persistent_attributes.
+        This is called monkey-patching, see
+        https://filippo.io/instance-monkey-patching-in-python/
+        """
+        if not new_module:
+            new_module = importlib.reload(sys.modules[self.__module__])
+        new_class = getattr(new_module, self.__class__.__name__)
+        persistent = {attr_name: getattr(self, attr_name, None)
+                      for attr_name in self._persistent_attributes}
+        for new_attr_name in dir(new_class):
+            if new_attr_name in ('__class__', '__dict__', '__weakref__'):
+                # do not copy '__dict__', '__weakref__'
+                # but copy '__class__' below
+                continue
+            new_attr = getattr(new_class, new_attr_name)
+            try:  # some attributes are instance methods, bind them to `self`
+                new_attr = types.MethodType(new_attr, self)
+            except TypeError:
+                pass
+            setattr(self, new_attr_name, new_attr)
+        setattr(self, '__class__', new_class)
+        new_class.__init__(self, **getattr(self, '_init_args', {}))
+        for k, v in persistent.items():
+            setattr(self, k, v)
+
+    def try_reload(self):
+        """
+        Try to reload the containing module.
+        If an exception is thrown in the process, catch and return it.
+        Useful to catch syntax errors.
+
+        :return the thrown exception if not successfully reloaded, None otherwise
+        """
+        try:
+            self.reload()
+        except Exception as e:
+            return e
+
+
+@pl_announce('ReloadTest')
+class ReloadTestPlugin(Reloadable, PluginBase):
+    requires = ('Event', 'TaskManager', 'Timers', 'Chat')
+    events = {'cmd_reload': 'reload_now'}
+
+    _persistent_attributes = ('event',)
+
+    def __init__(self, ploader, settings):
+        self.capture_args(locals())
+        super(ReloadTestPlugin, self).__init__(ploader, settings)
+        ploader.provides('ReloadTest', self)
+
+    def reload_now(self):
+
+        def unregister_handlers():
+            try:
+                for event, handler_name in self.events.items():
+                    handler = getattr(self, handler_name)
+                    if handler in self.event.event_handlers[event]:
+                        self.event.event_handlers[event].remove(handler)
+            except AttributeError:
+                pass
+
+        def do_the_reload():
+            unregister_handlers()
+            # setattr(self, 'reloaded', True)
+            e = self.try_reload()
+            if e is None:
+                unregister_handlers()
+                logger.debug('plugin reloaded')
+            else:
+                logger.debug('plugin not reloaded: %s', e)
+
+        def wait(time):
+            cb = lambda *_: self.event.emit('sleep_over', {})
+            self.timers.reg_event_timer(time, cb, runs=1)
+            return 'sleep_over'
+
+        def task():
+            logger.debug('before reload')
+            do_the_reload()
+            yield wait(1)
+            logger.debug('after reload')
+
+        self.taskmanager.run_task(task(), TaskChatter('Reloader', self.chat))
